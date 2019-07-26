0936035ae4ffd1c2d045150420a84982b3b14b87
SkinnyFatSumo <simon.coffin.website@gmail.com>
2019-07-25 18:05:25 -0700

    fixed url

--- src/actions/apiRoot.js
+++ src/actions/apiRoot.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-export const api_root = 'https://intense-reef-20993.herokuapp.com/';
-=======
 // export const api_root = 'https://aqueous-coast-67743.herokuapp.com/';
 export const api_root = 'https://intense-reef-20993.herokuapp.com/'
-// export const api_root = 'http://localhost:8000/';
->>>>>>> c482893fbba7f203ab0925abfd37c9b20375771b
